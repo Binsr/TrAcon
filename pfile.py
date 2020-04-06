0cd89d986a18265002729ddc5787e74925af6eda
Binsr <urosjovanovic.sna@gmail.com>
2020-04-06 14:51:41 +0200

    Update

--- pfile.py
+++ pfile.py
@@ -8,21 +8,35 @@ class Pfile:
         self.pathToDir= pathToDir
         self.filename= filename
 
-        info = self.getInfo(pathToDir + '/' + filename)
-        self.parentName= info[0]
-        self.parentEncoding= [1]
-        self.encoding= info[2]
+        self.parentName= None
+        self.parentEncoding= None
+        self.encoding= None
+        self.transArg= None #za izmenu da ne bude argument nego da bude
+
+        self.setInfo(pathToDir + '/' + filename)
+
         self.file= open(pathToDir + '/' + filename,'r')
         self.reader= pfReader.pfReder(self.file)
 
 
-    def getInfo(self,path):
+    def setInfo(self,path):
         markH= mark.Mark()
         info = markH.getInfo(path)
+        if len(info) != 4:
+            return
+
         info[1] = markH.decodeEn(info[1])
         info[2] = markH.decodeEn(info[2]) # ZA PREPRAVITI
-
-        return info
+        self.parentName= info[0]
+        self.parentEncoding= info[1]
+        self.encoding= info[2]
+        self.transArg= info[3]
 
     def readNext(self):
         return self.reader.readNext()
+
+    def getParentName(self):
+        return self.parentName
+
+    def getTransArg(self):
+        return self.transArg
--- updateHandle.py
+++ updateHandle.py
@@ -3,75 +3,120 @@ import pfReader
 import pfhandle
 import mark
 
+import pfile
+
 class UpdateHandle:
 
-    def __init__(self): #ako hoces kasnije rekurzivno ili nesto onda je bolje da se etuje preko update funkcije
-        self.path= None
-        self.parentFilePath= None
-        self.marks= None
+    def __init__(self):
+        pass
 
-    def updateDir(self, path, parentFilePath):
-        self.path= path
-        self.parentFilePath= parentFilePath
+    def updateDir(self, path, parentPath):
 
-        posOfName=  parentFilePath.rfind('/')+1
-        parentFileName= parentFilePath[posOfName:]
+        startPos = parentPath.rfind('/') + 1
+        parentName= parentPath[startPos:]
+
+        parentFile= pfile.Pfile(path,parentName)
+
+        childs= self.generateChildsArr(path,parentName)
+
+
+    def generateChildsArr(self, path, parentName):
+
+        filesArr = []  # Niz fajlova ciji je parent prozledjeni parent
 
         for filename in os.listdir(path):
-            if not filename.endswith(".properties") or filename == parentFileName:
+            if not filename.endswith(".properties") or filename == parentName:
                 continue
 
-            markF= mark.Mark()
-            self.marks= markF.getInfo(path + '/' + filename)
+            filesArr.append(pfile.Pfile(path, filename))
+            if filesArr[-1].getParentName() != parentName:
+                filesArr.pop()
 
-            if self.marks[0] != parentFileName:
-                continue
-            self.updateFile(path + '/' + filename)
+        return filesArr
 
-    def updateFile(self,filePath): #Da postoji klasa fajl ? koja ima mark ima en ima sve sto treba da ima ? moze da ima cak i readline
 
-        parentEn= self.marks[1]
-        childEn= self.marks[2]
-        transArg= self.marks[3]
+    def updateFile(self,file,parent):
 
-        markF= mark.Mark()
+        edtranslator= pfhandle.PFhandle(None,None,file.getTransArg()) #pf handle for encoding/decoding translation
 
-        parentEn= markF.decodeEn(self.marks[1])
-        childEn= markF.decodeEn(self.marks[2])
 
-        handleP= pfhandle.PFhandle(None,None,transArg) #Ovo mora da bude promenjivog tipa
 
-        cFile= open(filePath,'r')
-        pFile= open(self.parentFilePath,'r')
-        rFile= open(self.path + '/tmp.properties','w')
 
-        readerP= pfReader.pfReder(pFile)
-        readerC= pfReader.pfReder(cFile)
 
-        while True:
-            cLine= readerC.readNext()
-            if cLine is False:
-                break
 
-            if cLine['type'] == 'comment':
-                rFile.write(cLine['value'])
-            if cLine['type'] == 'property':
-                if not cLine['value']['string'].isspace():
-                    rFile.write(cLine['value']['property'] + cLine['value']['string'])
-                    rFile.write('\n\n')
-                else: #MALO LESPE OVO PREPRAVI
-                    while True:
-                        pLine= readerP.readNext()
-                        if pLine['type'] == 'property':
-                            if pLine['value']['property'] == cLine['value']['property']:
-                                rFile.write(pLine['value']['property'] + " ")
-                                rFile.write(handleP.translateLine(pLine['value']['string'],parentEn,childEn)) #ISTO PROMENJIVOG TIPA
-                                rFile.write("\n\n")
-                                break
-        rFile.close()
-        pFile.close()
-        rFile.close() #Dodaj da se prethodni fajl brise a tmp preimenuje u njegovo ime
 
-        return
 
-    #KLASA READ FILE KOJA ZNA KAKO DA CITA PROP I TAKO TO I DA IMA FUNCKIJU GET
+
+
+
+
+    # def __init__(self): #ako hoces kasnije rekurzivno ili nesto onda je bolje da se etuje preko update funkcije
+    #     self.path= None
+    #     self.parentFilePath= None
+    #     self.marks= None
+    #
+    # def updateDir(self, path, parentFilePath):
+    #     self.path= path
+    #     self.parentFilePath= parentFilePath
+    #
+    #     posOfName=  parentFilePath.rfind('/')+1
+    #     parentFileName= parentFilePath[posOfName:]
+    #
+    #     for filename in os.listdir(path):
+    #         if not filename.endswith(".properties") or filename == parentFileName:
+    #             continue
+    #
+    #         markF= mark.Mark()
+    #         self.marks= markF.getInfo(path + '/' + filename)
+    #
+    #         if self.marks[0] != parentFileName:
+    #             continue
+    #         self.updateFile(path + '/' + filename)
+    #
+    # def updateFile(self,filePath): #Da postoji klasa fajl ? koja ima mark ima en ima sve sto treba da ima ? moze da ima cak i readline
+    #
+    #     parentEn= self.marks[1]
+    #     childEn= self.marks[2]
+    #     transArg= self.marks[3]
+    #
+    #     markF= mark.Mark()
+    #
+    #     parentEn= markF.decodeEn(self.marks[1])
+    #     childEn= markF.decodeEn(self.marks[2])
+    #
+    #     handleP= pfhandle.PFhandle(None,None,transArg) #Ovo mora da bude promenjivog tipa
+    #
+    #     cFile= open(filePath,'r')
+    #     pFile= open(self.parentFilePath,'r')
+    #     rFile= open(self.path + '/tmp.properties','w')
+    #
+    #     readerP= pfReader.pfReder(pFile)
+    #     readerC= pfReader.pfReder(cFile)
+    #
+    #     while True:
+    #         cLine= readerC.readNext()
+    #         if cLine is False:
+    #             break
+    #
+    #         if cLine['type'] == 'comment':
+    #             rFile.write(cLine['value'])
+    #         if cLine['type'] == 'property':
+    #             if not cLine['value']['string'].isspace():
+    #                 rFile.write(cLine['value']['property'] + cLine['value']['string'])
+    #                 rFile.write('\n\n')
+    #             else: #MALO LESPE OVO PREPRAVI
+    #                 while True:
+    #                     pLine= readerP.readNext()
+    #                     if pLine['type'] == 'property':
+    #                         if pLine['value']['property'] == cLine['value']['property']:
+    #                             rFile.write(pLine['value']['property'] + " ")
+    #                             rFile.write(handleP.translateLine(pLine['value']['string'],parentEn,childEn)) #ISTO PROMENJIVOG TIPA
+    #                             rFile.write("\n\n")
+    #                             break
+    #     rFile.close()
+    #     pFile.close()
+    #     rFile.close() #Dodaj da se prethodni fajl brise a tmp preimenuje u njegovo ime
+    #
+    #     return
+    #
+    # #KLASA READ FILE KOJA ZNA KAKO DA CITA PROP I TAKO TO I DA IMA FUNCKIJU GET
